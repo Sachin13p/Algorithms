008ec07723072b41cf1f2f78113989fef7b2572a
Sachin13p <44912089+Sachin13p@users.noreply.github.com>
2019-10-30 15:08:48 -0700

    Added files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RatInTheMaze rt = new RatInTheMaze();
+            rt.main();
+            MultisourceTreasureIsland ms = new MultisourceTreasureIsland();
+            ms.Main(new string[] { });
+
+        }
+    }
+}
